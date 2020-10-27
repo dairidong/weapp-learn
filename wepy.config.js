d338c5d711025a1509b3970d8da85a4bf1d2ac0a
戴日东 <550398445@qq.com>
2020-10-27 15:45:25 +0800

    wepy login

--- wepy.config.js
+++ wepy.config.js
@@ -9,7 +9,6 @@ module.exports = {
   build: {},
   resolve: {
     alias: {
-      counter: path.join(__dirname, 'src/components/counter'),
       '@': path.join(__dirname, 'src')
     },
     aliasFields: ['wepy', 'weapp'],
