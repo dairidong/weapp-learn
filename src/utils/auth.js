c06d0680b2283c33cc168eb38d8ca860b3339657
戴日东 <550398445@qq.com>
2020-10-28 16:23:13 +0800

    authRequest and logout

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ export function getUser() {
   return wx.getStorageSync('user')
 }
 
-export function SetUser(user, perms) {
+export function setUser(user, perms) {
   return wx.setStorageSync('user', user)
 }
 
@@ -15,8 +15,8 @@ export function getTokenExpiredAt() {
 }
 
 export function setToken(tokenPayload) {
-  const accessToken = tokenPayload.data.access_token
-  const accessTokenExpiredAt = new Date().getTime() + tokenPayload.data.expires_in * 1000
+  const accessToken = tokenPayload.access_token
+  const accessTokenExpiredAt = new Date().getTime() + tokenPayload.expires_in * 1000
 
   wx.setStorageSync('access_token', accessToken)
   wx.setStorageSync('access_token_expired_at', accessTokenExpiredAt)
